09db23cd98737c981ec37ef5c8526636728e8584
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-09-20 21:46:44 -0400

    Access to *localhost:4000/secret* with 401 error, due to Login/Logout Issue while adding feature to authorize request in getInitialProps

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8969b012e1753851e7fc";
+/******/ 	var hotCurrentHash = "51b9588eba08a3627886";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- helpers/utils.js
+++ helpers/utils.js
@@ -5,7 +5,7 @@ export const getCookieFromReq = ( req, cookieKey ) => {
     const cookie = req.headers.cookie.split(';').find(c => c.trim().startsWith(`${cookieKey} = `));
     //Get cookie from header -> split it with ; -> look for the string starting with "cookie"
 
-    if(!cookie) {return undefined};
+    if(!cookie) { return undefined };
     // return undefined if there is no cookie
 
     return cookie.split('=')[1];// split() => returns array of expiresAt text, where 2nd value'[1]' is data
